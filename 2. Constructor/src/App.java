6bfcf620c6b6b94956a4c321e80af2c8e7fa7024
KLaZ <muhabdanulikhlas0983@gmail.com>
2023-03-06 14:19:09 +0700

    Menambahkan file Constructor terbaru

--- 2. Constructor/src/App.java
+++ 2. Constructor/src/App.java
@@ -1,23 +1,23 @@
-package com.tutorial;
 
-// class tanpa constructor / class polos
+
+//! class tanpa constructor / class polos
 class Polos{
     String dataString;
     int dataInteger;
 }
 
-// class dengan constructor
+//! class dengan constructor
 class Mahasiswa{
     String nama;
     String NIM;
     String jurusan;
 
-    // constructor dipanggil saat object pertama kali dibuat
+    //! constructor dipanggil saat object pertama kali dibuat
     // Mahasiswa(){
     //     System.out.println("ini adalah constructor");
     // }
 
-    // constructor dengan parameter
+    //! constructor dengan parameter
     Mahasiswa(String inputNama, String inputNIM, String inputJurusan){
         nama = inputNama;
         NIM = inputNIM;
@@ -29,7 +29,7 @@ class Mahasiswa{
     }
 }
 
-public class Main {
+public class App {
     public static void main(String[] args) throws Exception {
         
         Mahasiswa mahasiswa1 = new Mahasiswa("ucup","13305041","teknik perteknikan");
