3c78be455bfcd3bd48c4ed8478c5d45dda3f653a
KLaZ <muhabdanulikhlas0983@gmail.com>
2023-03-14 23:48:12 +0700

    update materi public and private keywords

--- 06. Public And Private Keywords/src/App.java
+++ 06. Public And Private Keywords/src/App.java
@@ -1,7 +1,7 @@
 class Player {
-    String name; // default, dia akan bisa dibaca dan ditulis dari luar class
-    public int exp; // public, dia akan bisa dibaca dan ditulis dari luar class
-    private int health; // private, hanya akan bisa dibaca dan ditulis di dalam class saja
+    String name; // ? default, dia akan bisa dibaca dan ditulis dari luar class
+    public int exp; // ? public, dia akan bisa dibaca dan ditulis dari luar class
+    private int health; // ? private, hanya akan bisa dibaca dan ditulis di dalam class saja
 
     Player(String name, int exp, int health) {
         this.name = name;
@@ -9,20 +9,20 @@ class Player {
         this.health = health;
     }
 
-    // default modifier access
+    // ! default modifier access
     void display() {
-        tambahExp(); // contoh mengakses private methods
+        tambahExp(); // ! contoh mengakses private methods
         System.out.println("\nName\t: " + this.name);
         System.out.println("exp\t: " + this.exp);
-        System.out.println("health\t: " + this.health); // membaca, tapi didalam class
+        System.out.println("health\t: " + this.health); // ! membaca, tapi didalam class
     }
 
-    // public
+    // ! public
     public void ubahName(String nameBaru) {
         this.name = nameBaru;
     }
 
-    // private
+    // ! private
     private void tambahExp() {
         this.exp += 100;
     }
@@ -33,31 +33,30 @@ public class App {
 
         Player player1 = new Player("Marni", 0, 100);
 
-        // default
-        System.out.println(player1.name); // membaca data
-        player1.name = "Surti"; // Menulis data
-        System.out.println(player1.name); // membaca data
+        // ! default
+        System.out.println(player1.name); // ? membaca data
+        player1.name = "Surti"; // ? Menulis data
+        System.out.println(player1.name); // ? membaca data
 
-        // public
-        System.out.println(player1.exp); // membaca data
-        player1.exp = 100; // Menulis data
-        System.out.println(player1.exp); // membaca data
+        // ! public
+        System.out.println(player1.exp); // ? membaca data
+        player1.exp = 100; // ? Menulis data
+        System.out.println(player1.exp); // ? membaca data
 
-        // private (tidak bisa diakses)
-        // System.out.println(player1.health); // membaca data
-        // player1.health = 200; // Menulis data
-        // System.out.println(player1.health); // membaca data
+        // !private (tidak bisa diakses di luar class)
+        // System.out.println(player1.health); //? membaca data
+        // player1.health = 200; //? Menulis data
+        // System.out.println(player1.health); //? membaca data
 
-        // methods
-
-        // default
+        // ! methods
+        // * default
         player1.display();
 
-        // public
+        // * public
         player1.ubahName("Tejo");
         player1.display();
 
-        // private (tidak bisa diakses)
+        // * private (tidak bisa diakses)
         // player1.tambahExp();
     }
 }
